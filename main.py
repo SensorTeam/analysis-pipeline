886a002a77f953901606240d035b0982bea9b407
CJ Melegrito <cj@mlgrto.com>
2018-06-23 12:44:04 +1000

    Quicksave

--- main.py
+++ main.py
@@ -22,41 +22,61 @@ def crop(image, factor):
 	crop_end = int(width * (trim + factor))
 	return image[0:height, crop_start:crop_end]
 
-def mask(image):
+def trim(image, factor):
+	width = image.shape[1]
+	trimmed_height = int(image.shape[0] * factor)
+	return image[0:trimmed_height, 0:width]
+
+def mask(image, factor):
 	gray_image = cv.cvtColor(image.copy(), cv.COLOR_BGR2GRAY)
-	return cv.threshold(gray_image, 200, 255, cv.THRESH_BINARY)[1]
+	return cv.threshold(gray_image, int(255 * factor), 255, cv.THRESH_BINARY)[1]
+
+def intensity(image):
+	width = image.shape[1]
+	height = image.shape[0]
+	gray_image = cv.cvtColor(image.copy(), cv.COLOR_BGR2GRAY)
+
+	score = 0
+
+	for row in range(0, height):
+		row_score = 0
+		for pixel in range(0, width):
+			row_score += gray_image[row][pixel]
+		score += row_score
+
+	return score
 
 def batch():
-	files = glob('data/*.JPG')
+	files = glob('data/*.jpg')
 	for file in files:
 		try:
 			image = read(file)
-			image = downscale(image, 0.25)
-			image = crop(image, 0.25)
-			write(image, file[:-4] + '_cropped.jpg')
-			image = mask(image)
+			# image = downscale(image, 0.25)
+			# image = crop(image, 0.1)
+			# image = trim(image, 0.6)
+			# write(image, file[:-4] + '_cropped.jpg')
+
+			# image = mask(image, 0.7) # 10-50m
+			image = mask(image, 0.4) # 60-100m
+			
 			write(image, file[:-4] + '_masked.jpg')
 
 		except IOError as e:
 			if e.errno != eerno.EISDIR:
 				raise
 
-def intensity(image):
-	# assumes image is cropped to spectra
+def batch_new():
+	files = glob('data/*.jpg')
+	for file in files:
+		try:
+			image = read(file)
+			print(file)
+			print(intensity(image))
 
-	width = image.shape[1]
-	height = image.shape[0]
-	gray_image = cv.cvtColor(image.copy(), cv.COLOR_BGR2GRAY)
 
-	for i in range(0, height):
-		# for each row
-		# get row average intensity and add it to total
-		for j in range(0, width):
-			# for each pixel
-			# get pixel 'score' between 0 to 255 and divide total 'score' by width
-			# add it to to the average row intensity
-
-	# divide total score by height to get intensity
+		except IOError as e:
+			if e.errno != eerno.EISDIR:
+				raise
 
 if __name__ == '__main__':
 	batch()
